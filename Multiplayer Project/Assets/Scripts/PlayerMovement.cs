ebc021af069ce0eb6b1214b9c8fb56a4daba57bb
Arrhythmia <mustafamuthenna@gmail.com>
2023-01-12 22:58:58 +0300

    Saved scene
    Saved scene and started preparing to work on player movement

--- Multiplayer Project/Assets/Scripts/PlayerMovement.cs
+++ Multiplayer Project/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,11 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
     [SerializeField] private float movementSpeed;
+    [SerializeField] private float jumpForce;
     private float movementDir;
+
+    [SerializeField] private float fallMultiplier = 2.5f;
+    [SerializeField] private float lowJumpMultiplier = 2f;
 
     private void Awake()
     {
@@ -25,9 +29,23 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(movementSpeed * movementDir, 0f, 0f, ForceMode.Force);
     }
 
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            Jump();
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
     private void FixedUpdate()
     {
-
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1f) * Time.deltaTime;
+        }
 
         MovePlayer(movementDir);
     }
